90466ddcebe924b595e2d5523f423c1a5bf926a3
mshort1803 <mshort1803@gmail.com>
2022-12-28 14:01:49 +1030

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  length: number = 0;
+  length: number = 9;
   includeLetters = false;
   includeNumbers = false;
   includeSymbols = false;
